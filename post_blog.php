ceb58f8ffaa8c5b485faea7456a0f3ea355bda8c
Aditya Kumar <adityakumar2004jsr@gmail.com>
2025-03-13 20:17:41 +0530

    Update post_blog.php

--- post_blog.php
+++ post_blog.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['post'])){
-$conn = mysqli_connect('129.154.240.37', 'aditya', 'password', 'database',3306) or die('Connection failed');
+$conn = mysqli_connect() or die('Connection failed');
 echo $blog_title = $_POST['title'];
 echo $blog_date = $_POST['date'];
 echo $blog_photo_url = $_POST['photo_url'];
